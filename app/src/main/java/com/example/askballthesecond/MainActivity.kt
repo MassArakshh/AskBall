d5a381359acbcc51d9f959c020ba96e164718dc6
MassArakshh <zdimonm@mail.ru>
2023-01-14 23:28:08 +0300

    4 BIG commit AscBall

--- app/src/main/java/com/example/askballthesecond/MainActivity.kt
+++ app/src/main/java/com/example/askballthesecond/MainActivity.kt
@@ -1,12 +1,56 @@
 package com.example.askballthesecond
 
+import android.content.Context
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.SystemClock
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
+
+
+class Fans {
+    fun bip2 (context: Context, sours: Int) {
+        var mediaPlayer = MediaPlayer.create(context,  sours)
+        mediaPlayer.start()
+    }
+}
+
+class Answers {
+
+    val ansHello = arrayOf("друг!", "чел!", "бро!", "пипл...")
+    val ansWisdom = arrayOf("Поспи.", "Поешь.", "Надо подумать!", "А надо ли?", "Тебе все по плечу!", "Это все не то...", "А как надо?",
+        "Это тайна\nпокрытая макром!", "Танцуй\nпока молодой!", "Включи Мозг!", "У мамы спроси.", "Плохо", "Думай", "Ага", "А как ты думаешь?")
+
+    fun choiseHelloAnswer (str: EditText): String {
+        val answer: String
+        val randomAnsHello = ansHello.random()
+
+        if ( str.text.isEmpty()){
+            if (randomAnsHello.equals("бро!") or randomAnsHello.equals("чел!") ) {
+                answer = "Дарова, " + randomAnsHello
+//                    SystemClock.sleep(2000)
+            } else
+                answer = "Привет, " + randomAnsHello
+        } else if (str.text.contains("Алиса") or str.text.contains("Lis")) {
+            answer = "Hello! " + str.text + ", доченька!"
+        } else if (str.text.contains("Маша")){
+            answer = "Ну привет, " + str.text +"."
+        } else {
+            answer = "Привет! " + str.text
+        }
+        return answer
+    }
+
+    fun choiseWisdomAnswer (): String {
+        val answer: String
+        val randomAnsWisdom = ansWisdom.random()
+        answer = randomAnsWisdom
+        return answer
+    }
+
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,52 +64,30 @@ class MainActivity : AppCompatActivity() {
 
         editText = findViewById(R.id.editPersonName)
         mHelloTextView = findViewById(R.id.textView)
-
         mAnswerView = findViewById(R.id.textView3)
 
 
-
-
         var imageButton: ImageButton = findViewById(R.id.imageButton)
+        var askButton: Button = findViewById(R.id.button)
+
         // альтернативный вариант
         // val imageButton = findViewById<ImageButton>(R.id.imageButton)
         // или val imageButton = findViewById(R.id.imageButton) as ImageButton
 
+        askButton.setOnClickListener {
+            askButton.setText("Думаю")
+            val myAnswers = Answers()
+            mAnswerView.text = myAnswers.choiseWisdomAnswer()
+        }
 
         imageButton.setOnClickListener {
-
-            val answers = arrayOf("друг!", "чел!", "бро!", "пипл...")
-            val randomAnswer = answers.random()
-
-            val answers2 = arrayOf("оспи.", "поешь.", "надо подумать!", "а надо ли?", "тебе все по плечу!", "это все не то...", "а как надо?",
-                "это тайна\nпокрытая макром!", "танцуй!", "включи Мозг!", "у мамы спроси.", "Плохо", "Думай", "Ага", "а как ты\nсама думаешь?")
-            val randomAnswer2 = answers2.random()
-
-
-            if (editText.text.isEmpty() or editText.text.endsWith("тут")){
-                if (randomAnswer.equals("бро!") or randomAnswer.equals("чел!") ) {
-                    mHelloTextView.text = "Дарова, " + randomAnswer
-                    SystemClock.sleep(2000)
-                   // mHelloTextView.text = "Включи мозг! "
-                } else
-                    mHelloTextView.text = "Привет! " + randomAnswer
-            } else if (editText.text.equals("Алиса")) {
-                mHelloTextView.text = "Hello! " + editText.text + ", доченька!"
-            } else if (editText.text.equals("Маша")){
-                mHelloTextView.text = "Ну привет, " + editText.text +"."
-            } else {
-                mHelloTextView.text = "Привет! " + editText.text
-            }
-
-
-            mAnswerView.text = randomAnswer2
-
-            //SystemClock.sleep(5000)
-//            return@setOnClickListener true
+            val myBip = Fans()
+            val myAnswers = Answers()
+            mHelloTextView.text = myAnswers.choiseHelloAnswer(editText)
+            myBip.bip2(applicationContext, R.raw.meu2)
         }
+
+
     }
 }
 
-private fun ImageButton.setOnLongClickListener(function: (View) -> Unit) {
-
-}
